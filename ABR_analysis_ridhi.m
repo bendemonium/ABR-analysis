ae4df5f75667c3f23d26f0e1858b8e7a7bb71b64
Ridhi Bandaru <ridhib2422@gmail.com>
2023-01-22 16:54:34 +0530

    input dialogue

--- ABR_analysis_ridhi.m
+++ ABR_analysis_ridhi.m
@@ -1,3 +1,13 @@
+prompt = {'Stimulus Frequency','Sample Rate','Response Threshold'};
+dlgtitle = 'Params';
+dims = [1 35];
+definput = {'4','20000','0.005'};
+answer = inputdlg(prompt,dlgtitle,dims,definput);
+stim_time  = 0.9 * (1/(str2num(answer{1})));
+sample_rate = str2num(answer{2});
+threshold = str2num(answer{3});
+min_int = stim_time*sample_rate;
+
 timemarks=[];
 stimulus = data(7:end,2); % channel 1
 response = data(7:end,3); % channel 3
@@ -5,9 +15,9 @@ rsv = zeros(200,1); % response sum vector
 
 %recording
 for j = 2:length(stimulus)
-    if stimulus(j) > 0.005
+    if stimulus(j) > threshold
         if ~isempty(timemarks)
-            if j - timemarks(end) > 2300 %time between stimulations, sample rate 10k
+            if j - timemarks(end) > min_int %time between stimulations, sample rate 10k
                 timemarks = [timemarks j];
             end   
         else timemarks = [timemarks j];
