69cdd02cc671db90b53a40c55d5f7a4722b18823
Ridhi Bandaru <ridhib2422@gmail.com>
2023-01-24 16:07:37 +0530

    Update ABR_analysis_ridhi.m

--- ABR_analysis_ridhi.m
+++ ABR_analysis_ridhi.m
@@ -5,8 +5,8 @@ dims = [1 35];
 definput = {'4','20000','0.005'};
 answer = inputdlg(prompt,dlgtitle,dims,definput);
 stim_time  = 0.9 * (1/(str2double(answer{1})));
-sample_rate = str2double(answer{2});
-threshold = str2double(answer{3});
+sample_rate = str2num(answer{2});
+threshold = str2num(answer{3});
 min_int = stim_time*sample_rate; %time between stimulations
 
 timemarks=[];
@@ -36,9 +36,11 @@ end
 
 %average plot
 av = rsv./length(timemarks);
+plot_lim = 1.2 * (max(abs(av)));
+plot_lim = abs(plot_lim);
 figure('name','AVERAGE RESPONSE','NumberTitle','off');
 plot(av, 'r','LineWidth',1.5)
-ylim([-0.0005 0.0005]);
+ylim([-(plot_lim) plot_lim]);
 title('Average Response Plot');
 ylabel('Response (V)');
 
